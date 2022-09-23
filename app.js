489a19f552a72045518653d264f66003a0446f9a
Sahil <leviack629@gmail.com>
2022-09-23 15:51:54 +0530

    Modified files

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-
-function saveLead() {
-    console.log("button clicked")
-}
-
-let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    console.log("button clicked")
+    
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}

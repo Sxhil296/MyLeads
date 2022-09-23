de1b368e847ab6754702720d6352f84f65ddf6a4
Sahil <leviack629@gmail.com>
2022-09-23 15:13:48 +0530

    Add Files

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+function saveLead() {
+    console.log("button clicked")
+}
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("button clicked")
+})
